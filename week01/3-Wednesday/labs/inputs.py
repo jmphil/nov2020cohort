cca1a5ad82dd55c2ad7c3a4e943f69edac2d5374
Matt Phillips <spectremp24@gmail.com>
2020-11-11 17:11:28 -0500

    commit for pull

--- week01/3-Wednesday/labs/inputs.py
+++ week01/3-Wednesday/labs/inputs.py
@@ -2,11 +2,22 @@
 # Please enter your name: Eric
 # Hello, Eric!
 
+first_name = input("What is your name?")
+
+print("Hello {}".format(first_name))
+
+
+
+
 
 # Prompt the user for his name using the input function. Example:
 
 
 # Same as the first exercise, but this time print the user's
 # name in ALL CAPS, and also tell them the number of letters in their name.
+
+first_name = input("What is your name?")
+
+print("Hello {}".format(first_name))
 
 # (Hint: You'll want to search for documentation on how to uppercase a string.)
