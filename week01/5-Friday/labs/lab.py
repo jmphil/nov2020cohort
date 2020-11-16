d833552fd484b7de24743fb0df132e0bf7d570db
Matt Phillips <spectremp24@gmail.com>
2020-11-16 14:09:24 -0500

    adding for pull

--- week01/5-Friday/labs/lab.py
+++ week01/5-Friday/labs/lab.py
@@ -2,60 +2,72 @@
 
 # 1. Write a function called "greeting" that prints "hello world" to the console
 
+# def greeting():
+#     print("Hello World")
+    
+
+    
+# for _ in range(5):
+#     print('Hi!')
+
+
+
 
 # 2. Execute (call/ run) the "greeting function"
 
-# 3. Reduce this code using functions
+# greeting()
 
-print("Day 1: Students in SRE class")
-print("lecture: git 101")
-print("Shu")
-print("Thomas")
-print("Gustavo")
-print("Alim")
-print("Day 2: Students in SRE class")
-print("lecture: git 102")
-print("Shu")
-print("Thomas")
-print("Gustavo")
-print("Alim")
-print("Day 3: Students in SRE class")
-print("lecture: python 101")
-print("Shu")
-print("Thomas")
-print("Gustavo")
-print("Alim")
+# # 3. Reduce this code using functions
+
+# def students():
+#     print("Shu")
+#     print("Thomas")
+#     print("Gustavo")
+#     print("Alim")
+    
+    
+# print("Day 1: Students in SRE class")
+# print("lecture: git 101")
+# students()
+# print("Day 2: Students in SRE class")
+# print("lecture: git 102")
+# students()
+# print("Day 3: Students in SRE class")
+# print("lecture: python 101")
+# students()
 
 
-# 4 Reduce the print statements for the  todolist code we worked on yesterday with a function
+#------------------------
 
-todos = []
-index = 0
-while True:
-    index = 0
-    task = input(
-        "What would you like to do?\n1. add a item\n2.remove item\n3.Print items\n4.exit\n>>>   ")
-    if int(task) == 1:
-        todoItem = input("Please enter a todo list item >>>   ")
-        todos.append(todoItem)
-        # add item
-    elif int(task) == 2:
-        # remove item
-        print('Which item do you want to delete?')
-        while index < len(todos):
-            # print(todos[index])
-            print(f'{index + 1}.{todos[index]}')
-            index += 1
-        delItem = input("item# >> ")
-        delItemNum = int(delItem)
-        del todos[delItemNum - 1]
-    elif int(task) == 3:
-        while index < len(todos):
-            print(todos[index])
-            index += 1
-        # print items
-    elif int(task) == 4:
-        break
+# # 4 Reduce the print statements for the  todolist code we worked on yesterday with a function
+
+# todos = []
+# index = 0
+# while True:
+#     index = 0
+#     task = input(
+#         "What would you like to do?\n1. add a item\n2.remove item\n3.Print items\n4.exit\n>>>   ")
+#     if int(task) == 1:
+#         todoItem = input("Please enter a todo list item >>>   ")
+#         todos.append(todoItem)
+#         # add item
+#     elif int(task) == 2:
+#         # remove item
+#         print('Which item do you want to delete?')
+#         while index < len(todos):
+#             # print(todos[index])
+#             print(f'{index + 1}.{todos[index]}')
+#             index += 1
+#         delItem = input("item# >> ")
+#         delItemNum = int(delItem)
+#         del todos[delItemNum - 1]
+#     elif int(task) == 3:
+#         while index < len(todos):
+#             print(todos[index])
+#             index += 1
+#         # print items
+#     elif int(task) == 4:
+#         break
 
 # 5. Nested Functions
 
@@ -64,30 +76,37 @@ while True:
 # The Parameters for the functions should be the first and last name person you
 # are recommending
 
-# Karen Jones
-# 1234 Park St
-# Anytown, Pennsylvania 12345
 
-# April 14, 2019
-
-# ABC College Admission’s Board
-# 1234 Butler Ave
-# Everywhere, CA 12345
-
-# Dear ABC College Admission’s Board:
-
-# My name is Karen Jones. I have served as a science teacher at Parktown High School for the past fifteen years and have had the privilege to serve as Ryan Thomas’s teacher for the past three. I have also been Ryan’s advisor on the science academic team here at school. Due to his qualifications, I feel that Ryan would be an excellent addition to your school.
-
-# While he has been a student here, Ryan has always challenged himself academically, taking all of the AP courses that our school has to offer. He has been captain of the academic team for the past two years, showing strong leadership qualities and organizational skills. His superior written and verbal skills have far surpassed any student of his age.
-
-# Ryan would bring much to your school, both in and out of the classroom. If you have any questions regarding Ryan’s qualifications, please contact me at (123) 555-5555 or at Karen.Jones@email.com.
-
-# Sincerely,
+def name(fist_name, last_name):
 
 
-# Karen Jones
-# Science Department Head
-# Park Town High School
+    print(f'''{first_name}{last_name}
+1234 Park St
+Anytown, Pennsylvania 12345
+
+April 14, 2019
+
+ABC College Admission’s Board
+1234 Butler Ave
+Everywhere, CA 12345
+
+Dear ABC College Admission’s Board:
+
+My name is {first_name} {last_name}. I have served as a science teacher at Parktown High School for the past fifteen years and have had the privilege to serve as Ryan Thomas’s teacher for the past three. I have also been Ryan’s advisor on the science academic team here at school. Due to his qualifications, I feel that Ryan would be an excellent addition to your school.
+
+While he has been a student here, Ryan has always challenged himself academically, taking all of the AP courses that our school has to offer. He has been captain of the academic team for the past two years, showing strong leadership qualities and organizational skills. His superior written and verbal skills have far surpassed any student of his age.
+
+Ryan would bring much to your school, both in and out of the classroom. If you have any questions regarding Ryan’s qualifications, please contact me at (123) 555-5555 or at {first_name}.{last_name}@email.com.
+
+Sincerely,
+
+
+{first_name} {last_name}
+Science Department Head
+Park Town High School''')
+
+    # name( Matt, Phillips)
+
 
 
 # 7. Order of parameters
@@ -121,3 +140,5 @@ while True:
 
 
 # Global Variables and Local Variables with the Same Names
+
+
