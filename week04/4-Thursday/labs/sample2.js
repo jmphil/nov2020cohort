7319133e674009d31b4132c55670dff173478640
Matt Phillips <spectremp24@gmail.com>
2020-12-04 10:13:14 -0500

    friday week 4

---
+++ week04/4-Thursday/labs/sample2.js
@@ -0,0 +1,99 @@
+// console.log("inside ext js file");
+
+// console.log("matt")
+// console.log("veronica")
+// console.log("kim")         
+// console.log("Micah")
+
+// var b = 3;
+    
+// console.log(b.lenght);
+// expr = "Papayas";
+
+// if(expr == 'Oranges')
+// {
+//     console.log('Oranges are $0.59 a pound.');
+// }
+// else if(expr == 'Mangoes')
+// {
+// }
+// else if(expr == 'Papayas')
+// {
+//     console.log('Mangoes and papayas are $2.79 a pound.');
+//     // expected output: "Mangoes and papayas are $2.79 a pound."
+// }
+// else
+// {
+//     console.log('Sorry, we are out of ' + expr + '.');
+// }
+// while loop----------------------
+// var count = 0
+// while (count < 10){
+
+//     count++;
+
+//     console.log(count);
+// }
+// for loop
+// for (var count = 0; count <= 10; count++){
+
+//     console.log(count);
+// }
+// var myArray = [1, 2, 3, 4, 5, 6];
+// console.log(myArray);
+// myArray.push(100)
+// console.log(myArray);
+// let alph= "abcdefghifklmnop";
+
+// let chars = alph.split ('');
+// console.log(chars);
+var num = 1234;
+
+var a = 9;
+//toString()
+//parseint()
+//split()
+//reverse()
+//join()
+// var splitString = num.split('num');
+// console.log(object);
+// var str= "hello"
+
+// function reverseString(str) {
+//     return str.split("").reverse().join("");
+// }
+// reverseString("hello");
+
+// var coins = [25, 10, 5]
+
+// make 50cents
+
+// var count = [25, 10, 5]
+// var amount = 40
+
+
+// while (count > 0){
+    
+//     console.log(count);
+// }
+
+// var phoneBook = {
+//     fName: "Just",
+//     lName: "Phillips",
+//     num: 7065555555
+// }
+// phoneBook['middleName'] = "Matthew"
+
+
+// delete phoneBook['lName']
+
+// console.log(phoneBook);
+
+function add(num1, num2){
+
+    return num1 + num2;
+}
+
+var result = add(4, 6)
+
+console.log(result);
