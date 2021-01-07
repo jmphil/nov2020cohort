f6aa876b0df2d39d224ca4bafaae7c9e58c8e2c4
Matt Phillips <spectremp24@gmail.com>
2021-01-07 12:04:37 -0500

    foo

--- week08/4-Thursday/labs/template/app.js
+++ week08/4-Thursday/labs/template/app.js
@@ -1,20 +1,22 @@
+
 const express = require('express');
 
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', 'views');
+
+//app.set('views', 'views')
+
 
 app.use(express.static('public'));
-app.use(require('./routes/index'));
 
-app.get('/', (req, res) => {
-    
-    res.send("who will win the battle?")
-})
+//sub routes
+app.use(require('./routes/index'));
+app.use(require('./routes/aboutus'));
+
 
 app.listen(3000, () => {
-
-    console.log.apply('Server running on port 3000')
-});
+  
+    console.log('Server is running on port 3000');
+})
 
