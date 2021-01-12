e290f59ba7e5d68165178e42809b0c91fbece64c
Matt Phillips <spectremp24@gmail.com>
2021-01-12 17:02:14 -0500

    started chat.js

---
+++ week08/4-Thursday/labs/artspeakers/routes/chat.js
@@ -0,0 +1,8 @@
+const socket = io();
+
+let chatUserName = document.querySelector('#chat-username');
+let chatMessage = document.querySelector('#chat-message');
+
+socket.on('connection', () => {
+   let chat 
+})
